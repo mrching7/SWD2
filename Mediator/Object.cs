2871b0d255109fc02d356f276955cb85344150fa
zabih <44346668+mrching7@users.noreply.github.com>
2019-10-10 21:38:38 +0200

    Add files via upload

---
+++ Mediator/Object.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public interface Object
+    {
+        //public TrafficLight t;
+        void CrossStreet();
+
+        int State();
+
+        int Location();
+        //public enum Currentstate
+        //{
+        //    Driveforward,
+        //    Driveright,
+        //    Driveleft
+        //};
+        //public enum CurrentLocation
+        //{
+        //    North=0,
+        //    South=1,
+        //    West=2,
+        //    East=3
+        //};
+    }
+
+    public class ConcreteCar : Object
+    {
+
+        private int state;
+        private int location;
+
+        public int State()
+        {
+             return state;
+        }
+        public int Location ()
+        {
+            return location;
+        }
+        public ConcreteCar()
+        {
+            Random rnd= new Random();
+
+            state = rnd.Next(4);
+
+            location = rnd.Next(5);
+
+           
+        }
+
+        public void CrossStreet()
+        {
+            if (location==0)
+            {
+                if (state==0)
+                {
+                    Console.WriteLine("Driving forward from north to south");
+                }
+                else if (state == 1)
+                {
+                    Console.WriteLine("Turning right from north");
+                }
+                else if (state == 2) 
+                {
+                    Console.WriteLine("turning left from north");
+                }
+            }
+            else if (location==1)
+            {
+                if (state == 0)
+                {
+                    Console.WriteLine("Driving forward from south to north");
+                }
+                else if (state == 1)
+                {
+                    Console.WriteLine("Turning right from south");
+                }
+                else if (state == 2)
+                {
+                    Console.WriteLine("turning left from south");
+                }
+            }
+            else if (location == 2)
+            {
+                if (state == 0)
+                {
+                    Console.WriteLine("Driving forward from West to east");
+                }
+                else if (state == 1)
+                {
+                    Console.WriteLine("Turning right from west");
+                }
+                else if (state == 2)
+                {
+                    Console.WriteLine("turning left from west");
+                }
+            }
+            else
+            {
+                if (state == 0)
+                {
+                    Console.WriteLine("Driving forward from east to west");
+                }
+                else if (state == 1)
+                {
+                    Console.WriteLine("Turning right from east");
+                }
+                else if (state == 2)
+                {
+                    Console.WriteLine("turning left from east");
+                }
+            }
+        }
+
+
+    }
+
+
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <ConcreteCar> C = new List<ConcreteCar>();
+            ConcreteCar C1=new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C2 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C3 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C4 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C5 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C6 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C7 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C8 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C9 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C10 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C11 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C12 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C13 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C14 = new ConcreteCar();
+            C.Add(C1);
+            C.Add(C2);
+            C.Add(C3);
+            C.Add(C4);
+            C.Add(C5);
+            C.Add(C6);
+            C.Add(C7);
+            C.Add(C8);
+            C.Add(C9);
+            C.Add(C10);
+            C.Add(C11);
+            C.Add(C12);
+            C.Add(C13);
+            C.Add(C14);
+
+            ConcreteTrafficLight concreteTraffic=new ConcreteTrafficLight(C);
+
+            concreteTraffic.NotifyForward();
+            Thread.Sleep(2000);
+            concreteTraffic.NotifyForward2();
+
+        }
+    }
+}
